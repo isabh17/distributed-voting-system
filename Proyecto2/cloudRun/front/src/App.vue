c8a0dfea2cc42d588527b44fd61f6e73910355b7
Isa <silverisa17@gmail.com>
2024-04-27 18:48:06 -0600

    Proyecto 2 - Sistemas Operativos 1

---
+++ Proyecto2/cloudRun/front/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>Logs</h1>
+    <table class="logs-table">
+      <thead>
+        <tr>
+          <th>Banda</th>
+          <th>Fecha y Hora</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item._id">
+          <td>{{ item.key }}</td>
+          <td>{{ item.Time }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <button @click="refreshData">Update Data</button>
+  </div>
+</template>
+
+
